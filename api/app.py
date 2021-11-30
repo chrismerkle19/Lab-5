ebc3ca6e764df936fb918a9bad441c05d6a8c00c
chrismerkle19 <82123634+chrismerkle19@users.noreply.github.com>
2021-11-30 15:41:12 -0600

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, json, request, jsonify
+from flask import Flask, request, jsonify
 import hashlib
 from math import sqrt
 from itertools import count, islice
 import requests
-from redis import Redis,StrictRedis,RedisError
+from redis import StrictRedis
 import os
 
 
-#Use strict redis
-redis = StrictRedis(host="127.0.0.1", port=6379, charset="utf-8", decode_responses=True)
+# Use strict redis
+r = StrictRedis('redis', 6379, charset="utf-8", decode_responses=True)
 app = Flask(__name__)
 
 
-#factorial
+# Factorial
 @app.route('/factorial/<int:num>')
 def factorial(num):
     temp_num = 1
@@ -36,13 +36,15 @@ def factorial(num):
             output=int(temp_num)
         )
 
-#fibonacci
+
+# Fibonacci
+
+
 @app.route("/fibonacci/<int:number>")
 def calc_fibonacci(number):
     fibonacci = [0]
     c1 = 0
     c2 = 1
-    fib = 0
     check = 0
 
     if number < 0:
@@ -60,13 +62,16 @@ def calc_fibonacci(number):
                 check = 1
     return jsonify(input=number, output=fibonacci)
 
-#is_prime
+# is_prime
+
+
 @app.route('/is-prime/<int:n>')
 def prime(n):
     return jsonify(
         input=n,
         output=is_prime(n)
     )
+
 
 def is_prime(n):
     if n < 2:
@@ -78,10 +83,12 @@ def is_prime(n):
 
     return True
 
-#md5
+# md5
+
+
 @app.route('/md5/<string:result>')
 def md5(result):
-    start  = result
+    start = result
     result = result
     result = hashlib.md5(result.encode())
     result = result.hexdigest()
@@ -91,14 +98,14 @@ def md5(result):
         output=result
     )
 
-#slack alert
+# slack alert
+
+
 @app.route('/slack-alert/<string:message>')
 def slackalert(message):
     payload = '{"text":"%s"}' % message
-    requests.post('https://hooks.slack.com/services/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', data=payload.encode('utf-8'))
-    return jsonify(input=message,
-        output=True)
-
+    requests.post(os.getenv('INPUT_SLACK_HOOK'), data=payload)
+    return jsonify(input=message, output=True)
 
 
 # LAB 6
@@ -125,7 +132,7 @@ def handle_post():
         ), 400
 
     # Check if key already exists
-    if redis.exists(k):
+    if r.exists(k):
         err_string = "Key already exists"
         return jsonify(
             key=k,
@@ -136,8 +143,8 @@ def handle_post():
         ), 409
 
     # Store data in redis
-    redis_result = redis.set(k, v)
-    if redis_result == False:
+    redis_result = r.set(k, v)
+    if not redis_result:
         err_string = "Could not write to DB"
     else:
         err_string = None
@@ -172,7 +179,7 @@ def handle_put():
         ), 400
 
     # Check if key doesn't exist
-    if redis.exists(k):
+    if r.exists(k):
         err_string = "Key already exists"
         return jsonify(
             key=k,
@@ -183,33 +190,38 @@ def handle_put():
         ), 409
     else:
         # store data in redis
-        redis_result = redis.set(k, v)
-        if redis_result == False:
-            err_string = "Could not write to DB"
-        else:
-            err_string = None
-            return jsonify(
-                key=k,
-                value=v,
-                command=f"CREATE {k}/{v}",
-                result=redis_result,
-                error=err_string
-            )
+        redis_result = r.set(k, v)
+        err_string = None
+        return jsonify(
+            key=k,
+            value=v,
+            command=f"CREATE {k}/{v}",
+            result=redis_result,
+            error=err_string
+        )
 
 
 # GET FUNCTION
 @app.route("/keyval/<string:key_string>", methods=["GET"])
 def get_value(key_string):
+    client_data = request.get_json()
+    v = client_data('value')
+    
     try:
-        temp_string = redis.get(key_string)
-    except redis.RedisError:
-        return jsonify(key=key_string,
-                       value=False,
-                       command="GET " + key_string,
-                       result=False,
-                       error="Invalid request"), 400
+        temp_string = r.get(key_string)
 
-    if redis.Redis.exists(key_string) == False:
+    except client_data.get('key'):
+        k = ''
+        err_string = "Invalid request from client"
+        return jsonify(
+            key=k,
+            value=v,
+            command=f"CREATE {k}/{v}",
+            result=False,
+            error=err_string
+        ), 400
+
+    if not r.exists(key_string):
         return jsonify(key=key_string,
                        value=temp_string,
                        command="GET " + key_string,
@@ -217,7 +229,7 @@ def get_value(key_string):
                        error="Key does not exist."), 404
 
     else:
-        val = redis.Redis.get(key_string)
+        val = r.get(key_string)
         return jsonify(key=key_string,
                        value=val,
                        command="GET " + key_string,
@@ -228,17 +240,25 @@ def get_value(key_string):
 # DELETE function
 @app.route("/keyval/<string:key_string>", methods=["DELETE"])
 def delete(key_string):
+    client_data = request.get_json()
+    v = client_data('value')
+    
     try:
-        temp_string = redis.get(key_string)
-    except redis.RedisError:
-        return jsonify(key=key_string,
-                       value=False,
-                       command="DELETE " + key_string,
-                       result=False,
-                       error="Invalid request"), 400
+        temp_string = r.get(key_string)
 
-    if redis.Redis.EXISTS(key_string):
-        redis.Redis.delete(key_string)
+    except client_data.get('key'):
+        k = ''
+        err_string = "Invalid request from client"
+        return jsonify(
+            key=k,
+            value=v,
+            command=f"CREATE {k}/{v}",
+            result=False,
+            error=err_string
+        ), 400
+
+    if r.exists(key_string):
+        r.delete(key_string)
         return jsonify(key=key_string,
                        value=temp_string,
                        command="DELETE " + key_string,
@@ -251,16 +271,6 @@ def delete(key_string):
                        result=False,
                        error="Key does not exist."), 404
 
-
-#{
-#    "success": True,
-#    "string": "user updated with ID 4",
-#    "id": 4
-#}
-#
-#{
-#    "message": "Invalid Application-Id or API-Key"
-#}
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
