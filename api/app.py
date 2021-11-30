dee9926cbc196dc2d3010cf0dcdf56e252d441a4
chrismerkle19 <82123634+chrismerkle19@users.noreply.github.com>
2021-11-29 21:05:45 -0600

    Update app.py

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ import os
 
 
 #Use strict redis
-redis = StrictRedis('redis',host="127.0.0.1", 6379, charset="utf-8", decode_responses=True)
+redis = StrictRedis(host="127.0.0.1", 6379, charset="utf-8", decode_responses=True)
 app = Flask(__name__)
 
 
